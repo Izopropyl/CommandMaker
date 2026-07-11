9f9b1d294a5a5755f94a54cfab8f5f5243696f31
Kingidk <kingidk@kingidk.net>
2026-07-11 10:53:16 -0400

    Fix Section Sign for legacy color codes

--- src/main/java/net/kingidk/commandMaker/commandcreation/ExecuteActions.java
+++ src/main/java/net/kingidk/commandMaker/commandcreation/ExecuteActions.java
@@ -81,8 +81,19 @@ public class ExecuteActions {
                 .replace("&c", "<red>").replace("&d", "<light_purple>")
                 .replace("&e", "<yellow>").replace("&f", "<white>")
                 .replace("&l", "<bold>").replace("&o", "<italic>")
-                .replace("&n", "<underlined>").replace("&m", "<strikethrough>")
-                .replace("&k", "<obfuscated>").replace("&r", "<reset>");
+                .replace("§n", "<underlined>").replace("§m", "<strikethrough>")
+                .replace("§k", "<obfuscated>").replace("§r", "<reset>")
+                .replace("§0", "<black>").replace("§1", "<dark_blue>")
+                .replace("§2", "<dark_green>").replace("§3", "<dark_aqua>")
+                .replace("§4", "<dark_red>").replace("§5", "<dark_purple>")
+                .replace("§6", "<gold>").replace("§7", "<gray>")
+                .replace("§8", "<dark_gray>").replace("§9", "<blue>")
+                .replace("§a", "<green>").replace("§b", "<aqua>")
+                .replace("§c", "<red>").replace("§d", "<light_purple>")
+                .replace("§e", "<yellow>").replace("§f", "<white>")
+                .replace("§l", "<bold>").replace("§o", "<italic>")
+                .replace("§n", "<underlined>").replace("§m", "<strikethrough>")
+                .replace("§k", "<obfuscated>").replace("§r", "<reset>");
     }
 
     private static String convertURLToClickable(String input) {
